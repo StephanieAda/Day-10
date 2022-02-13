ca7b23bec216fc8f668df1d2c5374960fda0245b
Timphanie <99142577+StephanieAda@users.noreply.github.com>
2022-02-13 23:42:42 +0000

    added docstrings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# Docstrings
 # functions with output
 
 def format_name(f_name,l_name):
+  """Take frst and last name and return title case version of the name."""
   if f_name == "" or l_name == "":
     return ("You didn't provide valid input")
   name = ""
@@ -9,3 +11,9 @@ def format_name(f_name,l_name):
   return name
 
 print(format_name(input("What is your first name? ") , input("What is your last name? ")))
+
+
+
+"""
+you can also use docstring to make comments, but thats too bothersome
+"""
